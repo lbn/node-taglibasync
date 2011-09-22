63e758c05342b9c99aa9b0c8570c270b9207a488
Liban Abdulkadir <liban@gmx.com>
2011-09-22 21:27:30 +0100

    Added ID3v2 support. The 'set' function now actually works.

--- src/taglib_async.cpp
+++ src/taglib_async.cpp
@@ -6,6 +6,12 @@
 
 #include <fileref.h>
 #include <tag.h>
+#include <mpegfile.h>
+#include <id3v2tag.h>
+#include <id3v2frame.h>
+#include <id3v2header.h>
+
+#include <id3v1tag.h>
 
 using namespace v8;
 using namespace node;
@@ -16,17 +22,21 @@ using namespace node;
                   String::New("Argument " #I " must be a function")));  \
   Local<Function> VAR = Local<Function>::Cast(args[I]);
 
-#define GET_STR_TAG(TAG) \
-	tagreq->taginfo->Set(String::New(#TAG),TagLibStringToString(tag->fileref->tag()->TAG())); 
-#define GET_INT_TAG(TAG) \
-	tagreq->taginfo->Set(String::New(#TAG),Integer::New(tag->fileref->tag()->TAG())); 
+#define SET_STR_TAG(TAG) \
+	v1obj->Set(String::New(#TAG),TagLibStringToString(mpeg->v1_tag->TAG())); 
+#define SET_INT_TAG(TAG) \
+	v1obj->Set(String::New(#TAG),Integer::New(mpeg->v1_tag->TAG())); 
 
-class Tag : ObjectWrap {
+class MPEG : ObjectWrap {
 public:
 	static Persistent<FunctionTemplate> pft;
-	TagLib::FileRef *fileref;
+	TagLib::MPEG::File *file;
+	TagLib::ID3v2::Tag *v2_tag;
+	TagLib::ID3v1::Tag *v1_tag;
+
 	Persistent<String> path;
 
+	
 	static void Initialize(Handle<Object> target)
 	{
 		HandleScope scope;
@@ -35,12 +45,12 @@ public:
 
 		pft = Persistent<FunctionTemplate>::New(t);
 		pft->InstanceTemplate()->SetInternalFieldCount(2);
-		pft->SetClassName(String::NewSymbol("Tag"));
+		pft->SetClassName(String::NewSymbol("MPEG"));
 
-		NODE_SET_PROTOTYPE_METHOD(pft,"get",Tag::get);
-		NODE_SET_PROTOTYPE_METHOD(pft,"set",Tag::set);
+		NODE_SET_PROTOTYPE_METHOD(pft,"get",MPEG::get);
+		NODE_SET_PROTOTYPE_METHOD(pft,"set",MPEG::set);
 
-		target->Set(String::NewSymbol("Tag"),pft->GetFunction());
+		target->Set(String::NewSymbol("MPEG"),pft->GetFunction());
 	}
 
 	static Handle<Value> New(const Arguments &args)
@@ -48,26 +58,26 @@ public:
 		HandleScope scope;
 		if (args.Length() < 1 || !args[0]->IsString())
 			return ThrowException(String::New("Expected string 'path' as first argument"));
-		Tag *tag = new Tag();
-		tag->path = Persistent<String>::New(args[0]->ToString());
-		tag->Wrap(args.This());
+		MPEG *mpeg = new MPEG();
+		mpeg->path = Persistent<String>::New(args[0]->ToString());
+		mpeg->Wrap(args.This());
 		return args.This();
 	}
-
+	
 	static Handle<Value> set(const Arguments &args)
 	{
 		HandleScope scope;
 		REQ_FUN_ARG(1,cb);
 		
-		Tag* tag = ObjectWrap::Unwrap<Tag>(args.This());
-		TagInfoReq *req = new TagInfoReq();
-		req->tag = tag;
+		MPEG* mpeg = ObjectWrap::Unwrap<MPEG>(args.This());
+		MPEGTagInfoReq *req = new MPEGTagInfoReq();
+		req->mpeg = mpeg;
 		req->cb = Persistent<Function>::New(cb);
 		req->taginfo = Persistent<Object>::New(Local<Object>::Cast(args[0]));
 		req->error = Persistent<Value>::New(Null());
-		tag->Ref();
+		mpeg->Ref();
 
-		eio_custom(Tag::EIO_set, EIO_PRI_DEFAULT, Tag::EIO_setAfter, req);
+		eio_custom(MPEG::EIO_set, EIO_PRI_DEFAULT, MPEG::EIO_setAfter, req);
 		ev_ref(EV_DEFAULT_UC);
 
 		return Undefined();
@@ -76,33 +86,41 @@ public:
 
 	static int EIO_set(eio_req *req)
 	{
-		TagInfoReq *tagreq = static_cast<TagInfoReq *>(req->data);
-		Tag *tag = tagreq->tag;
-		if (tag->fileref == NULL) {
-			String::Utf8Value path(tag->path);
-			tag->fileref = new TagLib::FileRef(*path);
-		}
-		if (tag->fileref->isNull() || !tag->fileref->tag()) {
-			tagreq->error	= Persistent<String>::New(String::New("Error while reading data from the file"));
-		} else {
-			Local<Array> fields = tagreq->taginfo->GetPropertyNames();
-			tagreq->error	= Persistent<Integer>::New(Integer::New(fields->Length()));
+		MPEGTagInfoReq *tagreq = static_cast<MPEGTagInfoReq *>(req->data);
+		MPEG *mpeg = tagreq->mpeg;
+		Read(mpeg);
+		if (mpeg->v2_tag && tagreq->taginfo->Has(String::New("v2"))) {
+			Local<Object> v2_obj = Local<Object>::Cast(tagreq->taginfo->Get(String::New("v2")));
+			Local<Array> fields = v2_obj->GetPropertyNames();
+			TagLib::ID3v2::FrameListMap framelistmap = mpeg->v2_tag->frameListMap();
+			
 			for (short i = 0; i < fields->Length(); i++)
 			{
-				Local<Value> field = capitalise(fields->Get(i));
-				Local<Value> val = tagreq->taginfo->Get(field);
-				// TODO: save changes
+				Local<String> field = Local<String>::Cast(fields->Get(i));
+				Local<Value> val = v2_obj->Get(field);
+				String::AsciiValue framename(field);
+				if (framelistmap.contains(*framename)) {
+					TagLib::ID3v2::Frame *frame = framelistmap[*framename].front();
+					frame->setText(NodeStringToTagLibString(val));
+				} else {
+					// TODO: create frame if not found
+				}
 			}
 		}
+		// TODO: Set v1 tags
+
+		if (!mpeg->file->save()) {
+			tagreq->error = Persistent<String>::New(String::New("Cannot save the file"));
+		}	
 		return 0;
 	}
 
 	static int EIO_setAfter(eio_req *req)
 	{
 		HandleScope scope;
-		TagInfoReq *tagreq = static_cast<TagInfoReq *>(req->data);
+		MPEGTagInfoReq *tagreq = static_cast<MPEGTagInfoReq *>(req->data);
 		ev_unref(EV_DEFAULT_UC);
-		tagreq->tag->Unref();
+		tagreq->mpeg->Unref();
 
 		Local<Value> argv[2];
 		argv[0] = Local<Value>::New(tagreq->error);
@@ -118,51 +136,82 @@ public:
 		return 0;
 	}
 
+	static bool Read(MPEG *mpeg)
+	{
+		if (mpeg->file) {
+			return true;
+		}
+		String::Utf8Value path(mpeg->path);
+		mpeg->file = new TagLib::MPEG::File(*path);
+		mpeg->v2_tag = mpeg->file->ID3v2Tag();
+		mpeg->v1_tag = mpeg->file->ID3v1Tag();
+
+		return true; // TODO: return false if file does not exist
+	}
 	static Handle<Value> get(const Arguments &args)
 	{
 		HandleScope scope;
 		REQ_FUN_ARG(0,cb);
 	
-		Tag* tag = ObjectWrap::Unwrap<Tag>(args.This());
-		TagInfoReq *req = new TagInfoReq();
-		req->tag = tag;
+		MPEG* mpeg = ObjectWrap::Unwrap<MPEG>(args.This());
+		MPEGTagInfoReq *req = new MPEGTagInfoReq();
+		req->mpeg = mpeg;
 		req->cb = Persistent<Function>::New(cb);
 		req->taginfo = Persistent<Object>::New(Object::New());
 		req->error = Persistent<Value>::New(Null());
-		tag->Ref();
+		mpeg->Ref();
 
-		eio_custom(Tag::EIO_get, EIO_PRI_DEFAULT, Tag::EIO_getAfter, req);
+		eio_custom(MPEG::EIO_get, EIO_PRI_DEFAULT, MPEG::EIO_getAfter, req);
 		ev_ref(EV_DEFAULT_UC);
 
 		return Undefined();
 	}
 	static int EIO_get(eio_req *req)
 	{
-		TagInfoReq *tagreq = static_cast<TagInfoReq *>(req->data);
-		Tag *tag = tagreq->tag;
-		if (tag->fileref == NULL) {
-			String::Utf8Value path(tag->path);
-			tag->fileref = new TagLib::FileRef(*path);
+		MPEGTagInfoReq *tagreq = static_cast<MPEGTagInfoReq *>(req->data);
+		MPEG *mpeg = tagreq->mpeg;
+		if (!Read(mpeg)) {
+			tagreq->error	= Persistent<String>::New(String::New("File does not exist"));
+			return 0;
 		}
-		if (tag->fileref->isNull() || !tag->fileref->tag()) {
-			tagreq->error	= Persistent<String>::New(String::New("Error while reading data from derp.ini"));
-		} else {
-			GET_STR_TAG(artist)
-			GET_STR_TAG(album)
-			GET_STR_TAG(title)
-			GET_STR_TAG(genre)
-			GET_INT_TAG(year)
-			GET_INT_TAG(track)
-		}	
+		if (!mpeg->v1_tag && !mpeg->v2_tag)	{
+			tagreq->error	= Persistent<String>::New(String::New("No ID3v1 or ID3v2 tags found"));
+			return 0;
+		}
+
+		Local<Object> v2obj = Object::New();
+		if (mpeg->v2_tag) {
+			TagLib::ID3v2::FrameList::ConstIterator it = mpeg->v2_tag->frameList().begin();
+			for(; it != mpeg->v2_tag->frameList().end(); it++) {
+				Local<String> frameid = TagLibStringToString(*(new TagLib::String((*it)->frameID())));
+				Local<Value> value = TagLibStringToString((*it)->toString());
+				printf("Frame: %s, Value: %s\n",*(String::AsciiValue(frameid)),*(String::AsciiValue(value)));
+				v2obj->ForceSet(frameid,value);
+			}
+		}
+		tagreq->taginfo->Set(String::New("v2"),v2obj);
+
+		Local<Object> v1obj = Object::New();
+		if (mpeg->v1_tag) {
+			SET_STR_TAG(title)
+			SET_STR_TAG(artist)
+			SET_STR_TAG(album)
+			SET_STR_TAG(comment)
+			SET_STR_TAG(genre)
+			SET_INT_TAG(track)
+			SET_INT_TAG(year)
+		}
+		tagreq->taginfo->Set(String::New("v1"),v1obj);
+	
 		return 0;
 	}
 
 	static int EIO_getAfter(eio_req *req)
 	{
 		HandleScope scope;
-		TagInfoReq *tagreq = static_cast<TagInfoReq *>(req->data);
+		MPEGTagInfoReq *tagreq = static_cast<MPEGTagInfoReq *>(req->data);
 		ev_unref(EV_DEFAULT_UC);
-		tagreq->tag->Unref();
+		tagreq->mpeg->Unref();
 
 		Local<Value> argv[2];
 		argv[0] = Local<Value>::New(tagreq->error);
@@ -190,35 +239,28 @@ public:
 	// Author: Nikhil Marathe (github/nikhilm)	
 	static TagLib::String NodeStringToTagLibString( Local<Value> s )
 	{
-		String::Utf8Value str(s->ToString());
+		String::AsciiValue str(s->ToString());
 		return TagLib::String(*str, TagLib::String::UTF8);
 	}
 
-	struct TagInfoReq
+	struct MPEGTagInfoReq
 	{
 		Persistent<Function> cb;
-		Tag *tag;
+		MPEG *mpeg;
 		Persistent<Object> taginfo;
 		Persistent<Value> error;
 	};
 
-	static Local<String> capitalise(Local<Value> val)
-	{
-		String::Utf8Value str(val);
-		std::string a = *str;
-		a[0] = toupper(a[0]);
-		return Local<String>::New(String::New(a.c_str()));
-	}
 };
 
 
 
-Persistent<FunctionTemplate> Tag::pft;
+Persistent<FunctionTemplate> MPEG::pft;
 
 extern "C" {
 	static void init(Handle<Object> target)
 	{
-		Tag::Initialize(target);
+		MPEG::Initialize(target);
 	}
 	NODE_MODULE(taglib_async, init);
 }
--- test.js
+++ test.js
@@ -1,10 +1,13 @@
 var taglib = require("./build/default/taglib_async.node");
 
-var tag = new taglib.Tag("/home/rusel/Music/Disturbed/Indestructible/Deceiver.mp3");
-tag.get(function(err,data) {
-	console.log(JSON.stringify(data));
-	tag.set({artist:"test"},function(err) {
-		console.log(err);
+var tag = new taglib.MPEG("/home/rusel/Music/Disturbed/Indestructible/Deceiver.mp3");
+
+var changes = {v2:{}};
+changes.v2.TIT2 = "TEST_Deceiver";
+changes.v2.TPE1 = "TEST_Disturbed";
+tag.set(changes,function(err){
+	tag.get(function(err2,data) {
+		console.log(JSON.stringify(data));
 	});
 });
 
